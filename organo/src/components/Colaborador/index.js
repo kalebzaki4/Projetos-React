5f22f9877b7027683bdd2049f85b50698ac14f3e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-08-02 11:45:36 -0300

    colocando o props de uma forma diferente

--- organo/src/components/Colaborador/index.js
+++ organo/src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = (props) => {
+const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
     return (
         <div className='colaborador'>
-            <div className='cabecalho'>
-                <img src={props.imagem} />
+            <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+                <img src={imagem} />
             </div>
             <div className='rodape'>
-                <h4>{props.nome}</h4>
-                <h5>{props.cargo}</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
