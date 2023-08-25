8d9d4f6c155f021adff1ec916615b6fcee26ac63
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-08-25 19:53:05 -0300

    Update index.jsx

--- Projeto-2/x-space/src/componentes/cabecalho/index.jsx
+++ Projeto-2/x-space/src/componentes/cabecalho/index.jsx
@@ -1,12 +1,13 @@
 import logo from './logo.png'
 import search from './search.png'
+import styles from './cabecalho.module.scss'
 
 export default function Cabecalho() {
     return(
-        <header>
+        <header className={styles.cabecalho}>
             <img src={logo} alt="Logo do X space" />
-            <div>
-                <input type="text" placeholder="O que você procura?"/>
+            <div className={styles.cabecalho__container}>
+                <input className={styles.cabecalho__input} type="text" placeholder="O que você procura?"/>
                 <img src={search} alt="icone de busca" />
             </div>
         </header>
