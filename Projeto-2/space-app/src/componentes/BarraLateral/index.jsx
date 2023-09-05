be0657b3d052c32d0934b30935fbcc2c9226373c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-05 11:28:46 -0300

    novo componente barra lateral

---
+++ Projeto-2/space-app/src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components"
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`
+
+const BarraLateral = () => {
+    return (
+        <aside>
+            <nav>
+                <ListaEstilizada>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Famosos</a></li>
+                </ListaEstilizada>
+            </nav>
+        </aside>
+    )
+}
+
+export default BarraLateral
