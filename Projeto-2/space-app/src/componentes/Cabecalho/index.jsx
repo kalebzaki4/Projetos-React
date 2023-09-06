a0197743f13fb55f35d6071a630b51fd97d3a437
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-06 11:13:42 -0300

    arrumei o cabecalho.

--- Projeto-2/space-app/src/componentes/Cabecalho/index.jsx
+++ Projeto-2/space-app/src/componentes/Cabecalho/index.jsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components"
-import CampoTexto from "../CampoTexto";
+import CampoTexto from "../CampoTexto"
 
 const HeaderEstilizado = styled.header`
     padding: 60px 0;
@@ -8,15 +8,13 @@ const HeaderEstilizado = styled.header`
     img {
         max-width: 212px;
     }
-`;
+`
 
 const Cabecalho = () => {
-    return (
-        <HeaderEstilizado>
-            <img src="/imagens/logo.png" alt="" />
-            <CampoTexto />
-        </HeaderEstilizado>
-    )
+    return (<HeaderEstilizado>
+        <img src="/imagens/logo.png" alt="" />
+        <CampoTexto />
+    </HeaderEstilizado>)
 }
 
 export default Cabecalho
