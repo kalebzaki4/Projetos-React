ecc89de7adacf139eb43cd8f51e6a805c8f1338b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-06 11:12:50 -0300

    adicionando os itens na BarraLateral

--- Projeto-2/space-app/src/componentes/BarraLateral/index.jsx
+++ Projeto-2/space-app/src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components"
+import ItemNavegacao from "./ItemNavegacao"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,8 +13,37 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Famosos</a></li>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/home-ativo.png" 
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-vistas-ativo.png" 
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-curtidas-ativo.png" 
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/novas-ativo.png" 
+                        iconeInativo="/icones/novas-inativo.png"
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/surpreenda-me-ativo.png" 
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
