48f56d965fd68b9130e8a5011324b4dc9a689c8f
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-07 19:45:37 -0300

    .

---
+++ Projeto-2/space-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { styled } from "styled-components"
+import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import bannerBackground from './assets/banner.png'
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+function App() {
+
+
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+      <BarraLateral />
+      <Banner
+        texto="A galeria mais completa de fotos do espaço!"
+        backgroundImage={bannerBackground}
+      />
+    </FundoGradiente>
+  )
+}
+
+export default App
