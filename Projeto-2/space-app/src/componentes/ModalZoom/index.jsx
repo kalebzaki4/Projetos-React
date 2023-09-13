de069b71bb6e29907816798a07fd3b67c69e0a8d
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-12 21:03:21 -0300

    novo componente do modalZoom

---
+++ Projeto-2/space-app/src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,14 @@
+const ModalZoom = ({ foto }) => {
+    return (
+        <>
+            <dialog open={true}>
+                <p>Greetings, one and all!</p>
+                <form action="dialog">
+                    <button>OK</button>
+                </form>
+            </dialog>
+        </>
+    )
+}
+
+export default ModalZoom
