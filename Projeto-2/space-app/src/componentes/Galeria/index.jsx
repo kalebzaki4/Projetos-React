9c048a5ec1ffd7ade277d6b34cdbb920e8394b05
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-12 21:01:40 -0300

    Update index.jsx

--- Projeto-2/space-app/src/componentes/Galeria/index.jsx
+++ Projeto-2/space-app/src/componentes/Galeria/index.jsx
@@ -1,9 +1,43 @@
-const Galeria = () => {
+import { styled } from "styled-components"
+import Titulo from "../Titulo"
+import Tags from "./Tags"
+import Populares from "./Populares"
+import Imagem from "./Imagem"
+
+const GaleriaContainer = styled.div`
+    display: flex;
+    gap: 24px;
+`
+
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`
+
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({ fotos = [] }) => {
     return (
-        <h2>
-            navegue pela galeria
-        </h2>
+        <>
+            <Tags />
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem 
+                            key={foto.id} 
+                            foto={foto} />)
+                        }
+                    </ImagensContainer>
+                </SecaoFluida>
+                <Populares />
+            </GaleriaContainer>
+        </>
     )
 }
 
-export default Galeria;
+export default Galeria
