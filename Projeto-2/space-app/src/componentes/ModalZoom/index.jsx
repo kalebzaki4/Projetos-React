d203867f8b83a973d2c6d203a11bb94387025c3c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-13 22:21:44 -0300

    dialog do modalZoom

--- Projeto-2/space-app/src/componentes/ModalZoom/index.jsx
+++ Projeto-2/space-app/src/componentes/ModalZoom/index.jsx
@@ -1,12 +1,48 @@
-const ModalZoom = ({ foto }) => {
+import { styled } from "styled-components"
+import Imagem from "../Galeria/Imagem"
+import BotaoIcone from "../BotaoIcone"
+
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+    background: transparent;
+    padding: 0;
+    border: 0;
+    width: 1156px;
+    display: flex;
+    justify-content: center;
+    form {
+        button {
+            position: relative;
+            top: 20px;
+            right: 60px;
+        }
+    }
+`
+
+const ModalZoom = ({ foto, aoFechar }) => {
     return (
         <>
-            <dialog open={true}>
-                <p>Greetings, one and all!</p>
-                <form action="dialog">
-                    <button>OK</button>
-                </form>
-            </dialog>
+            {foto && <>
+                <Overlay />
+                <DialogEstilizado open={!!foto} onClose={aoFechar}>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <BotaoIcone formMethod="dialog">
+                            <img src="/icones/fechar.png" alt="Icone de fechar" />
+                        </BotaoIcone>
+                    </form>
+                </DialogEstilizado>
+            </>}
         </>
     )
 }
