bf4b1e4c0e64a46b950f5a59921b8142236da3ff
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-19 22:29:23 -0300

    alterado 2 / 19/09/23

--- Projeto-3/ola-mundo/src/routes.js
+++ Projeto-3/ola-mundo/src/routes.js
@@ -1,7 +1,9 @@
+import PaginaPadrao from "componentes/PaginaPadrao";
+import Rodape from "componentes/Rodape";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
-import Inicio from './Paginas/Inicio';
-import SobreMim from './Paginas/SobreMim';
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
 
 function AppRoutes() {
   return (
@@ -9,10 +11,15 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
+        
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
