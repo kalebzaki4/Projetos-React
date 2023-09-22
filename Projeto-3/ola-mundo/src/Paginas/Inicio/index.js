7a5b6975b69feabf0f4f1f8a155504850c1b041f
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-21 21:08:07 -0300

    Update index.js

--- Projeto-3/ola-mundo/src/Paginas/Inicio/index.js
+++ Projeto-3/ola-mundo/src/Paginas/Inicio/index.js
@@ -1,7 +1,7 @@
 import styles from './Inicio.module.css';
 
 import posts from 'json/posts.json';
-import PostCard from 'componentes/PostCard';
+import PostCard from "componentes/PostCard";
 
 export default function Inicio() {
     return (
