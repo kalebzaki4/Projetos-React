d896a622e79e36ff600004ecf82deea0b8d38c64
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-29 13:03:17 -0300

    Update Card.jsx

--- Projeto-5/freelando/src/componentes/Card/Card.jsx
+++ Projeto-5/freelando/src/componentes/Card/Card.jsx
@@ -1,10 +1,10 @@
-import styled from "@emotion/styled"
+import styled from '@emotion/styled'
 
 const DivEstilizada = styled.div`
     padding: ${props => props.theme.espacamentos.l};
     background: ${props => props.theme.cores.secundarias.a};
     border: 1px solid;
-    border-color:  ${props => props.theme.cores.primarias.a};
+    border-color: ${props => props.theme.cores.primarias.a};
     border-radius: ${props => props.theme.espacamentos.s};
 `
 
