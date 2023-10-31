85f58cb2e90e91c407aec44fc5a15511d8732137
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-31 19:54:50 -0300

    Update router.jsx

--- Projeto-5/freelando/src/router/router.jsx
+++ Projeto-5/freelando/src/router/router.jsx
@@ -4,7 +4,25 @@ import PaginaInicial from "../paginas/PaginaInicial"
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        children: [
+            {
+                path: 'cadastro',
+                element: <h1>Seleção de tipo de cliente</h1>,
+                children: [
+                    {
+                        path: 'cliente',
+                        element: <h1>interesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluido</h1>
+                    },
+                ]
+            }
+        ],
     },
 ]);
