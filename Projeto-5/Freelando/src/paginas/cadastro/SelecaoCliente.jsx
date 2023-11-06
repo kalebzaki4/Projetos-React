3be8b517ee10255915fb1dbaf8063475d91bc030
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-11-06 10:53:19 -0300

    Update SelecaoCliente.jsx

--- Projeto-5/Freelando/src/paginas/cadastro/SelecaoCliente.jsx
+++ Projeto-5/Freelando/src/paginas/cadastro/SelecaoCliente.jsx
@@ -1,5 +1,16 @@
-const SelecaoCliente = () => {
+import Tipografia from "../../componentes/Tipografia/Tipografia"
 
+const SelecaoCliente = () => {
+    return(
+        <>
+            <Tipografia variante="h1" componente="h1">
+                Crie Seu cadastro
+            </Tipografia>
+            <Tipografia variante="h3" componente="h2">
+                Como podemos te ajudar?
+            </Tipografia>
+        </>
+    )
 }
 
 export default SelecaoCliente
