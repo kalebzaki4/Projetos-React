1b20ba1e1e6411b724d2224f0838e8a4ff835ac6
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-11 12:20:43 -0300

    Update App.js

--- Projeto-6/Alura-Books/src/App.js
+++ Projeto-6/Alura-Books/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Logo from './componentes/Logo'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Logo></Logo>
       </header>
     </div>
   );
