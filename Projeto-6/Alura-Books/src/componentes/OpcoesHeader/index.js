0d4ec402a999b25c6e7d376d3acaec6ff6d7f4b2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-15 17:08:58 -0300

    Update index.js

--- Projeto-6/Alura-Books/src/componentes/OpcoesHeader/index.js
+++ Projeto-6/Alura-Books/src/componentes/OpcoesHeader/index.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
-import './estilo.css'
 
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-
-const Opcao = styled.ul`
+const Opcao = styled.li`
     font-size: 16px;
     display: flex;
     justify-content: center;
@@ -19,13 +16,15 @@ const Opcoes = styled.ul`
     display: flex;
 `
 
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
 function OpcoesHeader() {
     return (
         <Opcoes>
-            {textoOpcoes.map((texto) => (
+            { textoOpcoes.map( (texto) => (
                 <Opcao><p>{texto}</p></Opcao>
-            ))}
-        </Opcoes>
+            ) ) }
+      </Opcoes>
     )
 }
 
