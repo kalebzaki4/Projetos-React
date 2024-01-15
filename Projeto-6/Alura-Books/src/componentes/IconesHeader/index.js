b9c203444a0436c100c4195690082d95a8e9e70b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-15 16:09:35 -0300

    Update index.js

--- Projeto-6/Alura-Books/src/componentes/IconesHeader/index.js
+++ Projeto-6/Alura-Books/src/componentes/IconesHeader/index.js
@@ -1,15 +1,21 @@
 import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+margin-right: 40px;
+width: 25px;
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
         <ul className='icones'>
-            { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
-            )) }
+            {icones.map((icone) => (
+                <li className='icone'><img src={icone}></img></li>
+            ))}
         </ul>
     )
 }
