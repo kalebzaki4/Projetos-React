b20d7f632e2e8ece7ec4ca29605053434cf83304
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-15 17:09:01 -0300

    Create index.js

---
+++ Projeto-6/Alura-Books/src/componentes/Pesquisa/index.js
@@ -0,0 +1,11 @@
+function Pesquisa() {
+    return (
+        <section>
+            <h2>Ja sabe né?</h2>
+            <h3>Procura ai bobao</h3>
+            <input placeholder="escreve ai bobao"/>
+        </section>
+    )
+}
+
+export default Pesquisa
