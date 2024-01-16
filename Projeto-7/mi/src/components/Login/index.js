453e10dcca1cafbd2622e84ae978879033fc81f8
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-16 20:33:22 -0300

    Update index.js

--- Projeto-7/mi/src/components/Login/index.js
+++ Projeto-7/mi/src/components/Login/index.js
@@ -1,6 +1,11 @@
 import Favicon from '../../assets/favicon.png'
+import './style.css'
+import { useState } from 'react'
 
 function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
     return (
         <div className="container">
             <div className="container-login">
@@ -10,8 +15,23 @@ function Login() {
                         <span className="login-form-title"><img src={Favicon} /></span>
 
                         <div className='wrap-input'>
-                            <input type='email' />
-                            <span className='focus-input'></span>
+                            <input className={email !== "" ? 'has-val input' : 'input' } type='email' value={email} onChange={e => setEmail(e.target.value)}/>
+                            <span className='focus-input' data-placeholder="Email"></span>
+                        </div>
+
+                        <div className='wrap-input'>
+                            <input className={password !== "" ? 'has-val input' : 'input' } type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+                            <span className='focus-input' data-placeholder="Password"></span>
+                        </div>
+
+                        <div className="container-login-form-btn">
+                            <button className="login-form-btn">Entrar</button>
+                        </div>
+
+                        <div className="text-center">
+                            <span className="txt1">Não possui conta?</span>
+
+                            <a className="txt2" href="#">Criar conta.</a>
                         </div>
 
                     </form>
