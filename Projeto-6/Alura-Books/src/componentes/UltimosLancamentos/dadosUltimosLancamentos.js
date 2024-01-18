147ba95e34cfc21dfcdcc1d21b7659a37143a55c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-18 10:12:38 -0300

    Create dadosUltimosLancamentos.js

---
+++ Projeto-6/Alura-Books/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,8 @@
+import livro from '../../Imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', src: livro, id: 1},
+    {nome: 'JavaScript Assertivo', src: livro, id: 2},
+    {nome: 'Cangaceiro Javascript: Uma aventura no sertão da programação', src: livro, id: 3},
+
+]
