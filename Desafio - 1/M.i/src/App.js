1430ef9583765b57d3a6416b0139135b0b197762
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-19 18:28:10 -0300

    Update App.js

--- Desafio - 1/M.i/src/App.js
+++ Desafio - 1/M.i/src/App.js
@@ -1,10 +1,19 @@
-import Login from './components/Login';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <>
-    <Login/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
