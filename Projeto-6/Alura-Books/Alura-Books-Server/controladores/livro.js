d73b630199c5e769945bb05998914b436c5a3af3
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-22 19:22:07 -0300

    Update livro.js

--- Projeto-6/Alura-Books/Alura-Books-Server/controladores/livro.js
+++ Projeto-6/Alura-Books/Alura-Books-Server/controladores/livro.js
@@ -1,6 +1,32 @@
+const { getTodosLivros, getLivroPorId, insereLivro } = require("../servicos/livro")
+
 function getLivros(req, res) {
     try {
-        res.send("Olá Mundo!")
+        const livros = getTodosLivros()
+        res.send(livros)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body
+        insereLivro(livroNovo)
+        res.send(201)
+        res.send("Livro inserido com sucesso")
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -8,5 +34,7 @@ function getLivros(req, res) {
 }
 
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro
 }
