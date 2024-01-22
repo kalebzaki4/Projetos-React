453254e6a5a631d6400d11a35dad29d4d52bdf21
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-22 19:22:11 -0300

    Update livro.js

--- Projeto-6/Alura-Books/Alura-Books-Server/rotas/livro.js
+++ Projeto-6/Alura-Books/Alura-Books-Server/rotas/livro.js
@@ -1,13 +1,14 @@
 const { Router } = require("express")
-const { getLivros } = require("../controladores/livro")
+const { getLivros, getLivro, postLivro} = require("../controladores/livro")
 
 const router = Router()
 
 router.get('/', getLivros)
 
-router.post('/', (req, res) => {
-    res.send('Você fez uma requisição do tipo POST')
-})
+router.get('/:id', getLivro )
+
+
+router.post('/', postLivro)
 
 router.patch('/', (req, res) => {
     res.send('Você fez uma requisição do tipo PATCH')
