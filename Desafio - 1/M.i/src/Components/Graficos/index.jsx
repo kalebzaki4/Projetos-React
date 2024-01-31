44127c5247dc7ab853e9ef7f283d2b6e33424ca3
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-01-31 17:50:40 -0300

    Update index.jsx

--- Desafio - 1/M.i/src/Components/Graficos/index.jsx
+++ Desafio - 1/M.i/src/Components/Graficos/index.jsx
@@ -6,10 +6,29 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';  // Adicione esta linha
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 import './style.css';
 
 import { Chart } from "react-google-charts";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 export default function MediaCard() {
   return (
@@ -76,37 +95,36 @@ export default function MediaCard() {
       </Card>
       <Card className='card2'>
         {/* Componente do primeiro gráfico */}
-        <Chart
-          chartType="LineChart"
-          width="100%"
-          height="300px"
-          data={[
-            ["x", "Ganhos", "Gastos"],
-            [0, 0, 0],
-            [1, 10, 5],
-            [2, 23, 15],
-            [3, 17, 9],
-            [4, 18, 10],
-            [5, 9, 5],
-            [6, 11, 3],
-            [7, 27, 19],
-          ]}
-          options={{
-            hAxis: {
-              title: "Dias",
-            },
-            vAxis: {
-              title: "Gastos",
-            },
-            series: {
-              1: { curveType: "function" },
-            },
-          }}
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Internet:
+            Gráfico dos Ganhos/Gastos:
           </Typography>
+          <Chart
+            chartType="AreaChart"
+            width="100%"
+            height="300px"
+            data={[
+              ["x", "Ganhos", "Gastos"],
+              [0, 0, 0],
+              [1, 10, 5],
+              [2, 23, 15],
+              [3, 17, 9],
+              [4, 18, 10],
+              [5, 9, 5],
+              [6, 11, 3],
+            ]}
+            options={{
+              hAxis: {
+                title: "Dias",
+              },
+              vAxis: {
+                title: "Gastos",
+              },
+              series: {
+                1: { curveType: "function" },
+              },
+            }}
+          />
           <Typography variant="body2" color="text.secondary">
             As informações do gráfico podem variar com a sua lista, sendo alteráveis. Você pode atualizá-las aqui conforme necessário.
           </Typography>
@@ -117,27 +135,27 @@ export default function MediaCard() {
       </Card>
       <Card className='card3'>
         {/* Componente do segundo gráfico */}
-        <Chart
-          chartType="PieChart"
-          width="100%"
-          height="300px"
-          data={[
-            ["Task", "Hours per Day"],
-            ["Soma das saidas", 11],
-            ["Aluguel", 4],
-            ["Conta de água", 2],
-            ["Conta de luz", 3],
-          ]}
-          options={{
-            title: "Analise dos dados:",
-            pieHole: 0.4,
-            is3D: false,
-          }}
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Visão geral:
+            Listas de gastos:
           </Typography>
+          <Chart
+            chartType="PieChart"
+            width="100%"
+            height="300px"
+            data={[
+              ["Task", "Hours per Day"],
+              ["Soma das saidas", 11],
+              ["Aluguel", 4],
+              ["Conta de água", 2],
+              ["Conta de luz", 3],
+            ]}
+            options={{
+              title: "Analise dos dados:",
+              pieHole: 0.4,
+              is3D: false,
+            }}
+          />
           <Typography variant="body2" color="text.secondary">
             As informações do gráfico podem variar com a sua lista, sendo alteráveis. Você pode atualizá-las aqui.
           </Typography>
@@ -146,15 +164,41 @@ export default function MediaCard() {
           <Button size="small">Saiba mais</Button>
         </CardActions>
       </Card>
-      <Card className='card3'>
+      <Card className='card4'>
         {/* Componente do terceiro gráfico */}
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Visão geral:
+            Lista de Gastos:
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            As informações do gráfico podem variar com a sua lista, sendo alteráveis. Você pode atualizá-las aqui.
-          </Typography>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
+                  <TableCell align="right">Calories</TableCell>
+                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </CardContent>
         <CardActions>
           <Button size="small">Saiba mais</Button>
