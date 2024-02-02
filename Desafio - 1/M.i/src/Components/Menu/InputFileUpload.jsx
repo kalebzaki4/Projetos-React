8e2b92ff40b88c8130bd01e33c01f99484c59a5b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-02-02 11:54:15 -0300

    Update InputFileUpload.jsx

--- Desafio - 1/M.i/src/Components/Menu/InputFileUpload.jsx
+++ Desafio - 1/M.i/src/Components/Menu/InputFileUpload.jsx
@@ -1,20 +1,40 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-const InputFileUpload = ({ onFileUpload }) => {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Handle the uploaded files (acceptedFiles)
-    onFileUpload(acceptedFiles);
-  }, [onFileUpload]);
+const InputFileUpload = ({ onFileUpload, onExcelRead, style }) => {
+  const [excelData, setExcelData] = useState([]);
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: '.csv, .xls, .xlsx, .txt' });
+  const onDrop = useCallback((acceptedFiles) => {
+    // Handle the uploaded files
+    console.log('Uploaded files:', acceptedFiles);
+    onFileUpload(acceptedFiles);
+
+    // Assume that the first file is an Excel file, adjust this logic as needed
+    const excelFile = acceptedFiles[0];
+    if (excelFile) {
+      const data = [['x', 'Ganhos', 'Gastos'], [0, 0, 0], [1, 10, 5], [2, 23, 15]];
+      console.log('Simulated Excel data:', data);
+      setExcelData(data);
+      onExcelRead(data);
+    }
+  }, [onFileUpload, onExcelRead]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: ['.csv', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', '.txt'],
+  });
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+      <Button
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+        style={{ backgroundColor: '#2C516B', color: '#FFFFFF', ...style }}
+      >
         Upload arq.
       </Button>
     </div>
