e715e2043b5ac4a2ce0a28af581f2894edf7c95b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-02-06 10:37:52 -0300

    Update dadosUltimosLancamentos.js

--- Projeto-6/Alura-Books/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++ Projeto-6/Alura-Books/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -1,8 +1,7 @@
-import livro from '../../Imagens/livro.png'
+import livro from '../../imagens/livro.png'
 
 export const livros = [
     {nome: 'Liderança em Design', src: livro, id: 1},
-    {nome: 'JavaScript Assertivo', src: livro, id: 2},
-    {nome: 'Cangaceiro Javascript: Uma aventura no sertão da programação', src: livro, id: 3},
-
+    {nome: 'Cangaceiro JavaScript: Uma aventura no sertão da programação', src: livro, id: 3},
+    {nome: 'Apache Kafka e Spring Boot', src: livro, id: 4}
 ]
