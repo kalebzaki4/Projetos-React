4eabccf14895d52da820fed730f64d55a8a6a337
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-02-13 20:51:47 -0300

    Update livro.js

--- Projeto-6/Alura-Books/Alura-Books-Server/rotas/livro.js
+++ Projeto-6/Alura-Books/Alura-Books-Server/rotas/livro.js
@@ -1,19 +1,16 @@
 const { Router } = require("express")
-const { getLivros, getLivro, postLivro, patchLivro} = require("../controladores/livro")
+const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require("../controladores/livro")
 
 const router = Router()
 
 router.get('/', getLivros)
 
-router.get('/:id', getLivro )
-
+router.get('/:id', getLivro)
 
 router.post('/', postLivro)
 
 router.patch('/:id', patchLivro)
 
-router.delete('/', (req, res) => {
-    res.send('Você fez uma requisição do tipo DELETE')
-})
+router.delete('/:id', deleteLivro)
 
 module.exports = router
