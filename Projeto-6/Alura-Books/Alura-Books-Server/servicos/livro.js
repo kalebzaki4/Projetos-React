fa0c44904b596a4920470ca68425a21299298ba2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-02-13 20:51:50 -0300

    Update livro.js

--- Projeto-6/Alura-Books/Alura-Books-Server/servicos/livro.js
+++ Projeto-6/Alura-Books/Alura-Books-Server/servicos/livro.js
@@ -1,20 +1,20 @@
 const fs = require("fs")
 
 function getTodosLivros() {
-    return JSON.parse(fs.readFileSync("livros.json"))
+    return JSON.parse( fs.readFileSync("livros.json") )
 }
 
 function getLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const livroFiltrado = livros.filter(livro => livro.id === id)[0]
+    const livroFiltrado = livros.filter( livro => livro.id === id )[0]
     return livroFiltrado
 }
 
 function insereLivro(livroNovo) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const novaListaDeLivros = [...livros, livroNovo]
+    const novaListaDeLivros = [ ...livros, livroNovo ]
 
     fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
@@ -30,6 +30,17 @@ function modificaLivro(modificacoes, id) {
     fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
 }
 
+function deletaLivroPorId(id) {
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const livrosFiltrados = livros.filter( livro => livro.id !== id )
+    fs.writeFileSync("livros.json", JSON.stringify(livrosFiltrados))
+}
+
 module.exports = {
-    getTodosLivros, getLivroPorId, insereLivro, modificaLivro
+    getTodosLivros,
+    getLivroPorId,
+    insereLivro,
+    modificaLivro,
+    deletaLivroPorId
 }
